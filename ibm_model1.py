384ff00b28ed5fbc7a0b2f6a5c184aa0838a48b0
Naren Surampudi <nsurampu@gmail.com>
2018-11-17 18:19:29 +0530

    Minor changes

--- ibm_model1.py
+++ ibm_model1.py
@@ -1,5 +1,6 @@
 import json
 import nltk
+from collections import defaultdict
 from pprint import pprint
 
 json_data_file = open("data1.json", 'r')
@@ -13,31 +14,32 @@ for bitext in json_data:
     e_sentence = bitext[languages[1]]
     for e_word in e_sentence.split(" "):
         for f_word in f_sentence.split(" "):
-            t[(e_word, f_word)] = 1.0
+            t[(e_word, f_word)] = 1/(len(languages[0]))
 
 iterations  = int(input("Enter iterations: "))
 
 for i in range(0, iterations):
-    count = {}
-    total_f = {}
-    total_e = {}
+    count = defaultdict(float)
+    total_f = defaultdict(float)
+    total_e = defaultdict(float)
     for bitext in json_data:
         f_sentence = bitext[languages[0]]
         e_sentence = bitext[languages[1]]
         for e_word in e_sentence.split(" "):
-            total_e[e_word] = 0.0
+            #total_e[e_word] = 0.0
             for f_word in f_sentence.split(" "):
                 total_e[e_word] = total_e[e_word] + t[(e_word, f_word)]
         for e_word in e_sentence.split(" "):
             for f_word in f_sentence.split(" "):
-                count[(e_word, f_word)] = 0.0
-                total_f[f_word] = 0.0
+                #count[(e_word, f_word)] = 0.0
+                #total_f[f_word] = 0.0
                 count[(e_word, f_word)] = count[(e_word, f_word)] + (t[(e_word, f_word)] / total_e[e_word])
                 total_f[f_word] = total_f[f_word] + (t[(e_word, f_word)] / total_e[e_word])
     word_pairs = list(t.keys())
     for f_word in set(total_f):
         for e_word in set(total_e):
             if (e_word, f_word) in word_pairs:
-                t[(e_word, f_word)] = count[(e_word, f_word)] / total_f[f_word]
+                if total_f[f_word] != 0:
+                    t[(e_word, f_word)] = count[(e_word, f_word)] / total_f[f_word]
 
 pprint(t)
