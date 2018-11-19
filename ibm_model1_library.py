ed4feec30fb3ff43223831578689ee7b44dfc481
aditya-srikanth <adityashrikanth14@gmail.com>
2018-11-19 19:35:03 +0530

    phrase ranking pending

--- ibm_model1_library.py
+++ ibm_model1_library.py
@@ -1,4 +1,5 @@
 from nltk import IBMModel1,IBMModel2,word_tokenize,AlignedSent
+from nltk.translate.phrase_based import phrase_extraction
 import json 
 from os import getcwd
 
@@ -17,6 +18,38 @@ if __name__ == '__main__':
     bitext = []
     for sentence in data1:
         bitext.append(AlignedSent(word_tokenize(sentence['fr'],language='french'),word_tokenize(sentence['en'],language='english')))
-
     model1 = IBMModel1(bitext,10)
+
+    print(bitext,'\n\n')
+
+    translate_table_1 = model1.translation_table
+
+    alignments_extracted_1 = []
+    for temp in bitext:
+        alignments_extracted_1.append(temp.alignment)
+    print(alignments_extracted_1)
+
+    bitext = []
+    for sentence in data1:
+        bitext.append(AlignedSent(word_tokenize(sentence['fr'],language='french'),word_tokenize(sentence['en'],language='english')))
     model2 = IBMModel2(bitext,10)
+    translate_table_2 = model2.translation_table
+
+    alignments_extracted_2 = []
+    for temp in bitext:
+        alignments_extracted_2.append(temp.alignment)
+    print('\n\n',alignments_extracted_2)
+
+
+    bitext_test = []
+    for sentence in data2:
+        bitext_test.append(AlignedSent(word_tokenize(sentence['fr'],language='french'),word_tokenize(sentence['en'],language='english')))
+    
+    # test model for extracting phrases, MODEL 1:
+    test_model = IBMModel1(bitext_test,10)
+    alignments_test = bitext_test[0].alignment
+    
+    # print(bitext_test[0],bitext_test[0].alignment)
+    phrases = phrase_extraction(data2[0]['fr'],data2[0]['en'],alignments_test)
+    # print(phrases)
+    
